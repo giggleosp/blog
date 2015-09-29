380c762287ec49da69b73a24a1dc6fc3fe0db1ea
enda <endaphelan1993@gmail.com>
2015-09-29 16:44:24 +0100

    first commit

---
+++ site-blog/install/info.php
@@ -0,0 +1,6 @@
+<?php if(!defined("PROCESSWIRE_INSTALL")) die();
+$info = array(
+	'title' => "Blog Profile", 
+	'summary' => "A response blog profile for ProcessWire 2.5 or newer.", 
+	'screenshot' => "screenshot.jpg"
+	);
