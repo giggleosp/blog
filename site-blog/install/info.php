042cdcaa4d6659ff838e3111d3462cf4f466edbb
enda <endaphelan1993@gmail.com>
2015-09-29 17:00:38 +0100

    added blog files and templates

--- site-blog/install/info.php
+++
@@ -1,6 +0,0 @@
-<?php if(!defined("PROCESSWIRE_INSTALL")) die();
-$info = array(
-	'title' => "Blog Profile", 
-	'summary' => "A response blog profile for ProcessWire 2.5 or newer.", 
-	'screenshot' => "screenshot.jpg"
-	);
